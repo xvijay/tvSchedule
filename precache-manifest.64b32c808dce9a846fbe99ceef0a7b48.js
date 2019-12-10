9a4a468db14ad1c61dc59f3ad9dbcdd1e4f72cb8
Vijay <vijay@pixels.agency>
2019-12-10 18:24:57 +0530

    Updates

--- precache-manifest.64b32c808dce9a846fbe99ceef0a7b48.js
+++ precache-manifest.64b32c808dce9a846fbe99ceef0a7b48.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0582a1ac7507f98fcaa2a1ff96c8ba01",
+    "revision": "81d7aa698ea43b39e1e3fb20af36ee87",
     "url": "/tvSchedule/index.html"
   },
   {
-    "revision": "9d202ddcacc915e2241e",
+    "revision": "974036fd3dbac9a55ba4",
     "url": "/tvSchedule/static/css/main.13605bb5.chunk.css"
   },
   {
-    "revision": "ee5d6239a00b9518ac69",
-    "url": "/tvSchedule/static/js/2.6238093f.chunk.js"
+    "revision": "0856a1e579df6e764af1",
+    "url": "/tvSchedule/static/js/2.b5c213af.chunk.js"
   },
   {
     "revision": "78ddf5a465591a03b9601ba2ddd63415",
-    "url": "/tvSchedule/static/js/2.6238093f.chunk.js.LICENSE"
+    "url": "/tvSchedule/static/js/2.b5c213af.chunk.js.LICENSE"
   },
   {
-    "revision": "9d202ddcacc915e2241e",
-    "url": "/tvSchedule/static/js/main.4af3ff8b.chunk.js"
+    "revision": "974036fd3dbac9a55ba4",
+    "url": "/tvSchedule/static/js/main.e8e8eb08.chunk.js"
   },
   {
     "revision": "0ecce7e2d07585f49aea",
