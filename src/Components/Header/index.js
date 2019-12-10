5c4b5dc4a2bc952e0f0b96a23f534538f8b4bc07
Vijay <vijay@pixels.agency>
2019-12-10 18:44:14 +0530

    imageLink updated

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,8 +4,11 @@ const header = props => {
   return (
     <section className="et-hero-tabs" ref={props.nav}>
       <h1 style={{ marginBottom: "1%" }}>
-        <img src="/image/television.png" style={{ height: "60px" }}></img> US TV
-        Schedule
+        <img
+          src="./image/television.png"
+          style={{ height: "60px" }}
+          raw={true}></img>{" "}
+        US TV Schedule
       </h1>
       <h6 style={{ margin: "unset" }}>One Stop Destination for TV Schedule</h6>
       <button className="headerButton" onClick={props.scrollFunction}>
