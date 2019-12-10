63d6b38627baa7d681378aa531c8deadda0068cc
Vijay <vijay@pixels.agency>
2019-12-10 19:08:27 +0530

    testing3

--- build/precache-manifest.a40b1f7531ea27e447fa610779ae8a01.js
+++ build/precache-manifest.a40b1f7531ea27e447fa610779ae8a01.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e32d6bedd7633074e533286e3ddd3b3",
+    "revision": "d3d624e5158387fc67db25429eafcea0",
     "url": "/tvSchedule/index.html"
   },
   {
-    "revision": "40d6c0ada84f43f1aa0b",
-    "url": "/tvSchedule/static/css/main.13605bb5.chunk.css"
+    "revision": "f1a4102a446ca4d5a873",
+    "url": "/tvSchedule/static/css/main.0f3189b5.chunk.css"
   },
   {
     "revision": "0856a1e579df6e764af1",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tvSchedule/static/js/2.b5c213af.chunk.js.LICENSE"
   },
   {
-    "revision": "40d6c0ada84f43f1aa0b",
+    "revision": "f1a4102a446ca4d5a873",
     "url": "/tvSchedule/static/js/main.a5c5408a.chunk.js"
   },
   {
